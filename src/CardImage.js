21ae559cf24c0091335b42b576d8e75e88d43e1a
Lauri Niskanen <ape@ape3000.com>
2024-03-21 01:14:30 +0200

    Adjust the Scryfall button position on split cards

--- src/CardImage.js
+++ src/CardImage.js
@@ -3,7 +3,7 @@ export default {
   template: `
   <div class="position-relative">
     <img :src="image" :alt="name" width="480" height="680" class="img-fluid m-auto" :class="{ 'img-thumbnail': size != 'small' }" @[card.can_flip&&"click"]="card.flipped ^= true">
-    <a v-if="!backside && size != 'small'" v-show="!card.flipped" :href="card.scryfall_url" target="_blank" class="scryfall-button" :class="{ 'scryfall-button-battle': card.types.includes('Battle') }">
+    <a v-if="!backside && size != 'small'" v-show="!card.flipped" :href="card.scryfall_url" target="_blank" class="scryfall-button" :class="{ 'scryfall-button-battle': card.types.includes('Battle'), 'scryfall-button-split': card.layout == 'split' }">
       <img src="images/scryfall.svg" alt="Scryfall" width="40" height="40">
     </a>
   </div>
